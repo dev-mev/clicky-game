41312bbc11dbbbb58275868353045ee056d59ba9
dev-mev <40147757+dev-mev@users.noreply.github.com>
2019-01-06 19:50:10 -0800

    Updates

---
+++ precache-manifest.ae9aad0529bd3152d13a75304bd855cf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "0ef12fe0166fd4efe268",
+    "url": "/clicky-game/static/js/main.0ef12fe0.chunk.js"
+  },
+  {
+    "revision": "ba1c9ecfabb80fadd3f0",
+    "url": "/clicky-game/static/js/1.ba1c9ecf.chunk.js"
+  },
+  {
+    "revision": "ca9f14487092f3356e64cb5f60c92353",
+    "url": "/clicky-game/index.html"
+  }
+];
